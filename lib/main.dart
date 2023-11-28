46bcc60dca13908e7be4e0581489b0aa3cd3c0ce
Mahesa Alfian Dhika <mahesaalfian03@gmail.com>
2023-11-28 14:05:07 +0700

    FIX : fix bug network_controller ( Ketika masuk ke aplikasi saat tanpa terhubung ke internet, network controller tidak berfungsi )

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:imp_approval/dependency_injection.dart';
 import 'package:imp_approval/layout/mainlayout.dart';
 import 'package:imp_approval/screens/login.dart';
+import 'package:imp_approval/controller/network_controller.dart';
 import 'package:imp_approval/splash_screen/splash.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/foundation.dart';
 import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'package:get/get.dart';
+import 'controller/store_manager.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   //Remove this method to stop OneSignal Debugging
@@ -17,15 +20,21 @@ Future<void> main() async {
 
   // The promptForPushNotificationsWithUserResponse function will show the iOS or Android push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission
   OneSignal.Notifications.requestPermission(true);
+  await StoreManager.getInstance();
 
+  DependencyInjection.init();
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   runApp(const MainApp());
-  DependencyInjection.init();
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -34,35 +43,45 @@ class MainApp extends StatelessWidget {
           SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
         },
         child: GetMaterialApp(
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            brightness: Brightness.light,
-            primarySwatch: Colors.blue,
-            visualDensity: VisualDensity.adaptivePlatformDensity,
-          ),
-          scrollBehavior: _getScrollBehavior(),
-          home: FutureBuilder(
-    future: SharedPreferences.getInstance(),
-    builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-            final token = (snapshot.data as SharedPreferences).getString('token');
-            if (token != null && token.isNotEmpty) {
-                return MainLayout();
-            } else {
-                return const LoginScreen();
-            }
-        } else if (snapshot.hasError) {
-            print(snapshot.error); // Debugging
-            return const Text('Some error has Occurred');
-        } else {
-            return const Center(
-                child: CircularProgressIndicator(),
-            );
-        }
-    }
-)
-
-        ));
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              brightness: Brightness.light,
+              primarySwatch: Colors.blue,
+              visualDensity: VisualDensity.adaptivePlatformDensity,
+            ),
+            scrollBehavior: _getScrollBehavior(),
+            home: Stack(
+              children: [
+                FutureBuilder(
+                    future: SharedPreferences.getInstance(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.done) {
+                        final token = (snapshot.data as SharedPreferences)
+                            .getString('token');
+                        if (token != null && token.isNotEmpty) {
+                          return MainLayout();
+                        } else {
+                          return const LoginScreen();
+                        }
+                      } else if (snapshot.hasError) {
+                        print(snapshot.error); // Debugging
+                        return const Text('Some error has Occurred');
+                      } else {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                    }),
+                Obx(() {
+                  if (!Get.find<NetworkController>().isConnected.value) {
+                    return Container(
+                      color: Colors.black.withOpacity(0.5),
+                    );
+                  }
+                  return SizedBox.shrink();
+                }),
+              ],
+            )));
   }
 
   ScrollBehavior _getScrollBehavior() {
@@ -72,7 +91,7 @@ class MainApp extends StatelessWidget {
       case TargetPlatform.android:
         return NoGlowBehavior();
       default:
-        return const MaterialScrollBehavior(); // Return the default behavior for other platforms
+        return const MaterialScrollBehavior();
     }
   }
 }
